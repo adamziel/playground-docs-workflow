968e33eb39ac63b404be3b1a593d19ecb767274e
Adam Zieliński <adam@adamziel.com>
2024-04-17 17:31:14 +0200

    Activate the plugins inside wp-docs-plugin

--- wp-content/plugins/wp-docs-plugin/plugin.php
+++ wp-content/plugins/wp-docs-plugin/plugin.php
@@ -63,6 +63,11 @@ function initialize_docs_plugin() {
     // Activating here because the activateTheme Blueprint step doesn't work
     // in wp-now :(
     switch_theme('playground-docs');
+    // Activate the gutenberg plugin and the create-block-theme plugin
+    // for the same reasons.
+    require ABSPATH . 'wp-admin/includes/plugin.php';
+    activate_plugin('gutenberg/gutenberg.php');
+    activate_plugin('create-block-theme/create-block-theme.php');
     pages_reinitialize_content();
 }
 
