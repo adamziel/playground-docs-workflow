ad58b797bfdbf71f8e53b588db18a84c2bfd55a4
Adam Zieliński <adam@adamziel.com>
2024-05-01 23:03:22 +0200

    Use @wp-playground.cli and the Simply Static plugin for static site generation (#59)
    This PR ships a Blueprint that generates the static site using the Simply Static plugin and the `run-blueprint` feature https://github.com/WordPress/wordpress-playground/pull/1289.
    
    Usage:
    
    ```shell
    bash build-static-site.sh
    # the site is now rendered in the output directory
    ```

---
+++ build-static-site.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p output
+bunx @wp-playground/cli@latest \
+    run-blueprint \
+    --mount=./wp-content/plugins/wp-docs-plugin:/wordpress/wp-content/plugins/wp-docs-plugin \
+    --mount=./wp-content/plugins/export-static-site:/wordpress/wp-content/plugins/export-static-site \
+    --mount=./wp-content/html-pages:/wordpress/wp-content/html-pages \
+    --mount=./wp-content/uploads:/wordpress/wp-content/uploads \
+    --mount=./wp-content/themes/playground-docs:/wordpress/wp-content/themes/playground-docs \
+    --mount=./output:/output \
+    --blueprint=./wp-content/blueprint-static-site.json \
+    --wp=6.5 \
+    --php=8.0 
+
+cd output
+unzip export.zip
+rm export.zip
